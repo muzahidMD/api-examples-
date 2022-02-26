c73d1eeee3e42f474264cd73cdb1418b91819c6e
Muzahidul Islam <mujahidul.core@gmail.com>
2022-02-26 17:43:44 +0600

    added some practice

--- js/countries.js
+++ js/countries.js
@@ -11,15 +11,38 @@ const displayCountries = countries => {
     // }
     const countriesDiv = document.getElementById('countries');
     countries.forEach(country => {
-        console.log(country);
+        // console.log(country);
         const div = document.createElement('div');
-        div.classList.add('country')
-        const h3 = document.createElement('h3');
-        h3.innerText = `Name: ${country.name.common}`;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = `Capital: ${country.capital}`
-        div.appendChild(p);
+        div.classList.add('country');
+        div.innerHTML = `
+            <h3>Name: ${country.name.common}</h3>
+            <p>Capital: ${country.capital}</p>
+            <button onclick="loadCountryByName('${country.name.common}')">Dtails </button>
+
+        `
+        // const h3 = document.createElement('h3');
+        // h3.innerText = `Name: ${country.name.common}`;
+        // div.appendChild(h3);
+        // const p = document.createElement('p');
+        // p.innerText = `Capital: ${country.capital}`
+        // div.appendChild(p);
         countriesDiv.appendChild(div);
     })
+}
+
+const loadCountryByName = name => {
+    const url = `https://restcountries.com/v3.1/name/${name}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCountryDtails(data[0]))
+}
+const displayCountryDtails = country => {
+    // console.log(country);
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+        <h5>${country.name.common}</h5>
+        <p>${country.population}</p>
+        <img src="${country.flags.png}">
+        
+    `
 }
---
+++ js/mealdbasync.js
@@ -0,0 +1,69 @@
+const searchFood = () => {
+    const searchField = document.getElementById('search-feild');
+    const searchText = searchField.value;
+    // console.log(searchText);
+    //cllear data
+    searchField.value = '';
+    if (searchResult == '') {
+        // please write something to display
+    }
+    else {
+        //load data
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => displaySearchResult(data.meals))
+    }
+
+
+}
+
+const displaySearchResult = meals => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    if (meals.length == 0) {
+        // 
+    }
+    meals.forEach(meal => {
+        // console.log(meal);
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card h-100">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title text-center">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0, 200)}</p>
+            </div>
+        </div>
+        `;
+        searchResult.appendChild(div);
+    })
+}
+
+const loadMealDetail = async mealId => {
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+    dispalyMealDetail(data.meals[0]);
+    // fetch(url)
+    //     .then(res => res.json())
+    //     .then(data => dispalyMealDetail(data.meals[0]))
+}
+const dispalyMealDetail = meal => {
+    console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.textContent = '';
+    const div = document.createElement('div');
+    div.classList.add('card')
+    div.innerHTML = `
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${meal.strMeal}</h5>
+            <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+            <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+        </div>
+    `;
+    mealDetails.appendChild(div)
+}
